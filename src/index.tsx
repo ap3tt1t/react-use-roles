031c387539a3e835c1860834022782683b8f1fbe
Andrew Pettit <pettit.andrew@gmail.com>
2021-08-31 22:06:55 +0200

    Fixed tiny problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,2 @@
 export {default as RoleChecker} from './components/RoleChecker'
-export {default as RolesProvider, useRolesHook as useRoles} from './context/index'
+export {default as RolesProvider, useRolesHook as useRoles} from './context/index' 
