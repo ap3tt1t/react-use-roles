8fb05664e57b4e8b05a86d2aab8e7b0d0c2c4afe
Andrew Pettit <pettit.andrew@gmail.com>
2021-09-02 09:27:11 +0200

    Added checking mode

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
-
+import './App.css'
 import { RoleChecker, RolesProvider, useRoles } from 'react-use-roles'
 
 const App = () => {
 	return <RolesProvider>
+		<h3 className='title'>REACT-USE-ROLES</h3>
+		<hr />
 		<RoleBasedComponent />
 	</RolesProvider>
 }
@@ -18,16 +20,46 @@ const RoleBasedComponent = () => {
 	}, [])
 	return (
 		<>
-			<RoleChecker allowed={['testers']}>
-				<p>Hello 1</p>
-			</RoleChecker>
-			<RoleChecker allowed={['testers', 'admin']}>
-				<p>Hello 1</p>
-			</RoleChecker>
-			<RoleChecker allowed={['accounts']} template={(<p>You can't access this</p>)}>
-				<p>Hello 2</p>
-				<p>Hello 2 again</p>
-			</RoleChecker>
+			<div className='section'>
+				<p className='header'>INCLUDES - role: 'admin', allowed: ['testers']:</p>
+				<p>EXPECTED</p>
+				<p className='check'>Hello 1</p>
+				<p>SHOWN:</p>
+				<RoleChecker allowed={['testers']} mode="includes">
+					<p className='check'>Hello 1</p>
+				</RoleChecker>
+			</div>
+			<hr />
+			<div className='section'>
+				<p className='header'>NONE - role: 'admin', allowed: ['testers']</p>
+				<p>EXPECTED</p>
+				<p className='check'>Hello 2</p>
+				<p>SHOWN:</p>
+				<RoleChecker allowed={['testers']} mode="none">
+					<p className='check'>Hello 2</p>
+				</RoleChecker>
+			</div>
+			<hr />
+			<div className='section'>
+				<p className='header'>DEFAULT (INCLUDES) - role: 'admin', allowed: ['testers', 'admin']</p>
+				<p>EXPECTED</p>
+				<p className='check'>Hello 3</p>
+				<p>SHOWN:</p>
+				<RoleChecker allowed={['testers', 'admin']}>
+					<p className='check'>Hello 3</p>
+				</RoleChecker>
+			</div>
+			<div className='section'>
+				<p className='header'>DEFAULT (INCLUDES), with TEMPLATE - role: 'admin', allowed: ['accounts']</p>
+				<p>EXPECTED</p>
+				<p className='check'>Hello 4</p>
+				<p className='check'>You can have multiple children elements</p>
+				<p>SHOWN:</p>
+				<RoleChecker allowed={['accounts']} template={(<p>You can't access this</p>)}>
+					<p className='check'>Hello 4</p>
+					<p className='check'>You can have multiple children elements</p>
+				</RoleChecker>
+			</div>
 		</>
 	)
 }
--- src/components/RoleChecker.tsx
+++ src/components/RoleChecker.tsx
@@ -2,16 +2,31 @@ import React from 'react'
 
 import {useRolesHook} from './../context'
 
+type ModeType = "includes" | "none"
+
 type RoleCheckerProps = {
     allowed: Array<string>
-    template?: object
+    template?: object,
+    mode?: ModeType
 }
 
-const RoleChecker:React.FC<RoleCheckerProps> = ({children, allowed, template}) => {
+const RoleChecker:React.FC<RoleCheckerProps> = ({children, allowed, template, mode="includes"}) => {
     const {role, defaultTemplate} = useRolesHook()
+    let isAllowed = false
+    switch (mode) {
+        case "includes": 
+            (role !== null && allowed.includes(role)) ? isAllowed = true : isAllowed = false;
+            break;
+        case "none": 
+            (role !== null && allowed.every(test => test !== role)) ? isAllowed = true : isAllowed = false;
+            break;
+        default: 
+            return null;
+            break;
+    }
     return (
         <React.Fragment>
-            {role !== null && allowed.includes(role) ? (<React.Fragment>{children}</React.Fragment>) : (
+            {isAllowed ? (<React.Fragment>{children}</React.Fragment>) : (
                 <React.Fragment>
                     {template !== undefined ? template : defaultTemplate}
                 </React.Fragment>
